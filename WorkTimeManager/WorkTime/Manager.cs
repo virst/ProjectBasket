963d5d93a4e993dbbdc1ffafeb08af1d1ede42a6
Virstyuk_MV <Virstyuk_MV@ASU-05-VMV>
2017-09-27 16:06:50 +0500

    Новый

---
+++ WorkTimeManager/WorkTime/Manager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkTime
+{
+    public class Manager
+    {
+        public readonly List<Work> works = new List<Work>();
+        public string FN = "Works.wtm";
+        public void Load()
+        {
+            var ss = File.ReadAllLines(FN);
+            works.Clear();
+            foreach (string s in ss)
+            {
+                works.Add(new Work(s));
+            }
+        }
+
+        public void AddInFile(Work w)
+        {
+            File.AppendAllText(FN,w + Environment.NewLine);
+        }
+
+        public float SumByMonth(int m)
+        {
+            Load();
+            return works.Where(w => w.moment.Month == m).Sum(w => w.Time);
+        }
+
+        public class Work
+        {
+            private const char Splitter = '\t';
+            public static readonly CultureInfo cf = new CultureInfo("en-US");
+            public Work()
+            {
+            }
+
+            public Work(string s)
+            {
+                var ss = s.Split(Splitter);
+                Time = Convert.ToSingle(ss[0], cf);
+                Type = ss[1][0];
+                moment = Convert.ToDateTime(ss[2]);
+            }
+
+            public float Time;
+            public char Type;
+            public DateTime moment;
+
+            public override string ToString()
+            {
+                return Time.ToString(cf) + Splitter + Type + Splitter + moment.ToString("d");
+            }
+        }
+
+
+    }
+}
---
+++ WorkTimeManager/WorkTimeManager/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkTime;
+
+namespace WorkTimeManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float t = 0;
+            int m = DateTime.Now.Month;
+            Manager mn = new Manager();
+            var tmpColor = Console.ForegroundColor;
+          
+            Manager.Work w = new Manager.Work();
+
+            do
+            {
+                Console.Clear();
+                t = mn.SumByMonth(m);
+                Console.ForegroundColor = ConsoleColor.Green;;
+                Console.WriteLine("Month = {0};Total time = {1:F2}",m,t);
+                Console.ForegroundColor = tmpColor;
+                Console.WriteLine("Укажите отработку в формате T 0.00 (T - код отработки; 0.00 - затраченое время)");
+                var s = Console.ReadLine();
+                w.Type = s[0];
+                w.moment = DateTime.Today;
+                w.Time = Convert.ToSingle(s.Split(' ')[1], Manager.Work.cf);
+                mn.AddInFile(w);
+
+
+            } while (t >= 0);
+            Console.WriteLine("End!");
+        }
+    }
+}
