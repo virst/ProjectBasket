76e7e9a0d6c38fc544904dd5c6de60f01190df76
Virstyuk_MV <Virstyuk_MV@ASU-05-VMV>
2017-10-30 17:37:26 +0500

    Автоматическое применения параметров

---
+++ AutoParam/AutoParam/AutoParamAdapter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoParam
+{
+    public class AutoParamAdapter<T>
+    {
+        public static void SetParams(string[] args)
+        {
+            var tp = typeof(T);
+
+            foreach (string a in args)
+            {
+                var pp = a.Split('=');
+                if(pp.Length!=2)
+                    continue;
+
+                var f = tp.GetField(pp[0]);
+                f.SetValue(null, Convert.ChangeType(pp[1],f.FieldType));
+            }
+        }
+    }
+}
---
+++ AutoParam/AutoParam/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoParam
+{
+    class Program
+    {
+        public static int ii=13;
+        public static int ab=99;
+        public static int f=1;
+        public static DateTime dt=new DateTime(2015,4,5);
+        public static string msg = "Hello";
+
+        static void Main(string[] args)
+        {
+            AutoParamAdapter<Program>.SetParams(args);
+
+            Console.WriteLine("ii = {0}", ii);
+            Console.WriteLine("ab = {0}", ab);
+            Console.WriteLine("f = {0}", f);
+            Console.WriteLine("msg = {0}", msg);
+            Console.WriteLine("dt = {0}", dt.ToLongDateString());
+
+            Console.ReadKey();
+        }
+    }
+}
